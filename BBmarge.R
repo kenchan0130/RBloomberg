6b99a9f247ed37117226c73a8a1e52b076b85fe9
kenchan0130 <tt.tanishi100@gmail.com>
2013-05-06 22:48:55 +0900

    change algorithm, return as list

--- BBmarge.R
+++ BBmarge.R
@@ -1,4 +1,5 @@
 BB.marge <- function(csv.text,xts=FALSE){
+	
 	if(xts){
 		library(xts)
 	}
@@ -9,61 +10,40 @@ BB.marge <- function(csv.text,xts=FALSE){
 	else if(length(grep("csv\\>",csv.text))==0){
 		stop("\ncsvのファイル名を入力してください")
 	}
-
-	x = read.csv(csv.text)
 	
-	if(ncol(x)==2){
-		cat("\nデータ数が1つであるため整形のみを行いました")
-		x = x[-1,]
-		x.name = colnames(x)[1]
-		Date.name = as.Date(x[,1])
-		x = data.frame(as.numeric(as.character(x[,-1])))
-		colnames(x) = x.name
-		rownames(x) = Date.name
-		if(xts){
-			x = as.xts(x)
-		}
-		return(x)
+	trim <- function(obj) {
+		obj[-which(obj[,1]==""),]
 	}
-	
-	x = x[,-seq(3,ncol(x),by=3)]
-	x = x[-1,]
+
+	x    = read.csv(csv.text, stringsAsFactors=FALSE)
+	x.na = which(is.na(x[1,]))
+	x    = x[,-x.na]
 	n.col = ncol(x)
+	col.name = x[1,]
+	date.vec = grep("\\<Date\\>", col.name)
+	x.name = colnames(x)[date.vec]
+	
+	x = data.frame(x[-1,])
+	colnames(x) = col.name
+	
 
-	x.name = colnames(x)[-seq(2,n.col,by=2)]
-	colnames(x)[seq(1,n.col,by=2)] = "Date"
-	x = apply(x,2,function(x) replace(x,which(x==""),NA))
-
-
-	tmp  = merge(na.omit(x[,1:2]),na.omit(x[,3:4]),by="Date",all=TRUE)
-	data = tmp[order(as.Date(tmp$Date)),]
-
-	if(n.col==4){
-		Date.name = data$Date
-		data = data[,-1]
-		data = apply(data,2,function(x) as.numeric(as.character(x)))
-		colnames(data) = x.name
-		rownames(data) = Date.name
-		return(data)
-	}
-
-	rm(tmp)
-
-	for(j in seq(5,n.col,by=2)){
-		a    = merge(data,na.omit(head(x[,j:(j+1)])),by="Date", all=TRUE, sort=FALSE)
-     		data = a[order(as.Date(a$Date)),]
-     		gc();gc();
-     		rm(a)
-	}
-
-
-	Date.name      = data$Date
-	data           = data[,-1]
-	data           = apply(data,2,function(x) as.numeric(as.character(x)))
-	colnames(data) = x.name
-	rownames(data) = Date.name
-
-	if(xts)	data = as.xts(data)
-
-	data
+	param.n = diff(date.vec)-1
+	if (sum(param.n==param.n[1])!=length(param.n==param.n[1])) stop("\nデータの個数がそれぞれ異なっている可能性があります")
+	kekka = lapply(seq(param.n[1]), function(n){
+				data = trim(x[,c(1,n+1)])
+				for(i in date.vec[-1]) {
+					tmp           = trim(x[,c(i,i+n)])
+					tmp.colnames  = colnames(tmp)
+					colnames(tmp)[2] = paste(tmp.colnames[2],i,sep="")
+					data.tmp = merge(data, tmp, by="Date",all=TRUE)#,incomparables=NA)
+					data     = data.tmp[order(as.Date(data.tmp$Date)),]
+				}
+				Date.name      = data$Date
+				data           = data[,-1]
+				colnames(data) = col.name
+				rownames(data)   = Date.name
+				if(xts) data     = as.xts(data)
+				return(data)
+			})
+	return(kekka)
 }
