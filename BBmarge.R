378a8b8c37b1856661d346082fb7f8cd346a8d3d
kenchan0130 <tt.tanishi100@gmail.com>
2013-05-06 23:58:33 +0900

    bug fix

--- BBmarge.R
+++ BBmarge.R
@@ -12,7 +12,13 @@ BB.marge <- function(csv.text,xts=FALSE){
 	}
 	
 	trim <- function(obj) {
-		obj[-which(obj[,1]==""),]
+		if(is.character(obj[,1])){
+			result = obj[-which(obj[,1]==""),]
+		}
+		else{
+			result = obj[-which(is.na(obj[,1])),]
+		}
+		return(result)
 	}
 
 	x    = read.csv(csv.text, stringsAsFactors=FALSE)
@@ -31,18 +37,19 @@ BB.marge <- function(csv.text,xts=FALSE){
 	if (sum(param.n==param.n[1])!=length(param.n==param.n[1])) stop("\nデータの個数がそれぞれ異なっている可能性があります")
 	kekka = lapply(seq(param.n[1]), function(n){
 				data = trim(x[,c(1,n+1)])
+				
 				for(i in date.vec[-1]) {
 					tmp           = trim(x[,c(i,i+n)])
 					tmp.colnames  = colnames(tmp)
 					colnames(tmp)[2] = paste(tmp.colnames[2],i,sep="")
-					data.tmp = merge(data, tmp, by="Date",all=TRUE)#,incomparables=NA)
-					data     = data.tmp[order(as.Date(data.tmp$Date)),]
+					data = merge(data, tmp, by="Date",all=TRUE)
 				}
+				data           = data[order(as.Date(data$Date)),]
 				Date.name      = data$Date
 				data           = data[,-1]
 				colnames(data) = x.name
-				rownames(data) = Date.name
-				if(xts) data   = as.xts(data)
+				rownames(data)   = Date.name
+				if(xts) data     = as.xts(data)
 				return(data)
 			})
 	return(kekka)
