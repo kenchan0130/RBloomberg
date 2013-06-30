8fa01e931e9e63d77b9c2c2f5d6673c0af3cfd63
kenchan0130 <tt.tanishi100@gmail.com>
2013-06-30 09:46:30 +0900

    change algorithm which is used by xts.

--- BBmarge.R
+++ BBmarge.R
@@ -1,56 +1,47 @@
-BB.marge <- function(csv.text,xts=FALSE){
-	
-	if(xts){
-		library(xts)
+library(xts)
+library(compiler)
+BB.merge <- function(filename, xts=TRUE){
+	if (is.na(pmatch("package:xts", search()))){
+		stop("\nYou must load 'xts' package.")
+	}
+	if (!is.character(filename)){
+		stop("\nfilename isn't character.")
 	}
 	
-	if(!is.character(csv.text)){
-		stop("\ncsv.textの引数は文字列のみです")	
-	}
-	else if(length(grep("csv\\>",csv.text))==0){
-		stop("\ncsvのファイル名を入力してください")
-	}
+	x         = read.csv(filename, stringsAsFactors=FALSE)
+	name      = colnames(x)
+	na.info   = which(is.na(x[1,]))
+	param.num = na.info[1]-2
+	start     = na.info-(param.num+1)
+	start     = c(start, start[length(start)]+na.info[1])
+	end       = na.info-1
+	end       = c(end, end[length(end)]+na.info[1])
+	data.num  = length(start)
+	x.onlynum = x[-1,]
+	my.as.xts <- function(df) {
+		index = which(as.character(df[,1])!="")
+		date  = as.character(df[index,1])
+		df    = apply(df[index,],1,function(x) as.numeric(x[-1]))
+		df    = as.matrix(df)
+		rownames(df) = date
+		as.xts(df)
+	} 
 	
-	trim <- function(obj) {
-		if(is.character(obj[,1])){
-			result = obj[-which(obj[,1]==""),]
+	result = lapply(seq(param.num), function(pn){
+		cat(sprintf("############ %s/%s parameter ############\n",pn,param.num))
+		cat(sprintf("%s/%s times process\n",1,data.num-1))
+		kekka = merge(my.as.xts(x.onlynum[ c(start[1],(start[1]+pn)) ]),my.as.xts(x.onlynum[ c(start[2],(start[2]+pn)) ]),join="outer")
+		
+		for(i in seq(3,data.num)){
+			cat(sprintf("%s/%s times process\n",i-1,data.num-1))
+			kekka = merge(kekka,my.as.xts(x.onlynum[ c(start[i],(start[i]+pn)) ]))
 		}
-		else{
-			result = obj[-which(is.na(obj[,1])),]
-		}
-		return(result)
-	}
+		gc();gc();
+		colnames(kekka) = name[start]
+		if(!xts) kekka = data.frame(kekka)
+		kekka
+	})
 
-	x    = read.csv(csv.text, stringsAsFactors=FALSE)
-	x.na = which(is.na(x[1,]))
-	x    = x[,-x.na]
-	n.col = ncol(x)
-	col.name = x[1,]
-	date.vec = grep("\\<Date\\>", col.name)
-	x.name = colnames(x)[date.vec]
-	
-	x = data.frame(x[-1,])
-	colnames(x) = col.name
-	
-
-	param.n = diff(date.vec)-1
-	if (sum(param.n==param.n[1])!=length(param.n==param.n[1])) stop("\nデータの個数がそれぞれ異なっている可能性があります")
-	kekka = lapply(seq(param.n[1]), function(n){
-				data = trim(x[,c(1,n+1)])
-				
-				for(i in date.vec[-1]) {
-					tmp           = trim(x[,c(i,i+n)])
-					tmp.colnames  = colnames(tmp)
-					colnames(tmp)[2] = paste(tmp.colnames[2],i,sep="")
-					data = merge(data, tmp, by="Date",all=TRUE)
-				}
-				data           = data[order(as.Date(data$Date)),]
-				Date.name      = data$Date
-				data           = data[,-1]
-				colnames(data) = x.name
-				rownames(data)   = Date.name
-				if(xts) data     = as.xts(data)
-				return(data)
-			})
-	return(kekka)
+	result
 }
+BB.merge=cmpfun(BB.merge)
